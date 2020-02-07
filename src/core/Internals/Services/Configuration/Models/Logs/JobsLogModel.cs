dc6c6c401da7dda5bf8db8994f7ab91205ab65a6
huseyin uslu <shalafiraistlin@gmail.com>
2020-02-07 13:00:32 +0300

    fixed missing files.

---
+++ src/core/Internals/Services/Configuration/Models/Logs/JobsLogModel.cs
@@ -0,0 +1,25 @@
+﻿#region license
+// 
+//      hypeengine
+// 
+//      Copyright (c) 2016 - 2019, Int6ware
+// 
+//      This file is part of hypeengine project. Unauthorized copying of this file, via any medium is strictly prohibited.
+//      The hypeengine or its components/sources can not be copied and/or distributed without the express permission of Int6ware.
+#endregion
+
+namespace Homer.Core.Internals.Services.Configuration.Models.Logs
+{
+    public class JobsLogModel
+    {
+        /// <summary>
+        /// Console log.
+        /// </summary>
+        public ConsoleLogModel Console { get; set; }
+
+        /// <summary>
+        /// File log.
+        /// </summary>
+        public FileLogModel File { get; set; }
+    }
+}
